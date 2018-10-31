cc0e664f5f0e44e9f976462a702b61fc54cf252f
AnkitDroidGit <ankitdroiddeveloper@gmail.com>
2018-10-31 18:21:23 +0800

    process image

---
+++ ObjectDetectInImage/Utils.swift
@@ -0,0 +1,26 @@
+//
+//  Utils.swift
+//  ObjectDetectInImage
+//
+//  Created by Ankit Kumar on 31/10/2018.
+//  Copyright © 2018 Ankit Kumar. All rights reserved.
+//
+
+
+import UIKit
+
+// Convert UIImageOrientation to CGImageOrientation for use in Vision analysis.
+extension CGImagePropertyOrientation {
+    init(_ uiImageOrientation: UIImage.Orientation) {
+        switch uiImageOrientation {
+        case .up: self = .up
+        case .down: self = .down
+        case .left: self = .left
+        case .right: self = .right
+        case .upMirrored: self = .upMirrored
+        case .downMirrored: self = .downMirrored
+        case .leftMirrored: self = .leftMirrored
+        case .rightMirrored: self = .rightMirrored
+        }
+    }
+}
