33797eb71bcaada0f63b9385a2ddfc3ff8af23a7
AnkitDroidGit <ankitdroiddeveloper@gmail.com>
2018-10-31 16:27:16 +0800

    Initial Commit

---
+++ ObjectDetectInImage/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ObjectDetectInImage
+//
+//  Created by Ankit Kumar on 31/10/2018.
+//  Copyright © 2018 Ankit Kumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
